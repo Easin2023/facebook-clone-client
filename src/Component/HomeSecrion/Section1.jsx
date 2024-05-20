1935e9937622e7fe0dd606bd14500cf141166952
Easin2023 <mdeasinarafat016456@gmail.com>
2024-05-20 20:33:25 +0600

    added to story section

--- src/Component/HomeSecrion/Section1.jsx
+++ src/Component/HomeSecrion/Section1.jsx
@@ -4,60 +4,60 @@ import { IoIosArrowDown } from "react-icons/io";
 const Section1 = () => {
   return (
     <div>
-      <div className="flex flex-col w-full mt-10">
-        <button className="flex items-center justify-start btn w-full">
+      <div className="flex flex-col w-full mt-5">
+        <button className="flex items-center bg-[#F0F2F5] justify-start btn w-full">
           <img
-            className="w-10 rounded-full"
+            className="w-8 rounded-full"
             src="https://i.ibb.co/5MWWZD7/394559949-374692381622478-7628019852304827346-n-1-removebg-preview-1.png"
             alt=""
           />
-          <span>Easin Arafat</span>
+          <span className="pl-4">Easin Arafat</span>
         </button>
-        <button className="flex items-center justify-start btn w-full">
+        <button className="flex bg-[#F0F2F5] items-center justify-start btn w-full">
           <img
             className="w-12"
             src="https://i.ibb.co/zHdx3bt/images-removebg-preview.png"
             alt=""
           />
-          <span>Friends</span>
+          <span className="">Friends</span>
         </button>
-        <button className="flex items-center btn justify-start w-full">
+        <button className="flex bg-[#F0F2F5] items-center btn justify-start w-full">
           <img
             className="w-10"
             src="https://i.ibb.co/f2gJ7fm/images-2-removebg-preview-1.png"
             alt=""
           />
-          <span>Feeds</span>
+          <span className="pl-2">Feeds</span>
         </button>
-        <button className="flex items-center btn justify-start w-full">
+        <button className="flex bg-[#F0F2F5] items-center btn justify-start w-full">
           <img
             className="w-10"
             src="https://i.ibb.co/VppHNLn/images-1-removebg-preview.png"
             alt=""
           />
-          <span>Groups</span>
+          <span className="pl-2">Groups</span>
         </button>
-        <button className="flex items-center btn justify-start w-full">
+        <button className="flex bg-[#F0F2F5] items-center btn justify-start w-full">
           <img
             className="w-9"
             src="https://i.ibb.co/71hJtxj/images-2-removebg-preview.png"
             alt=""
           />
-          <span>Marketplace</span>
+          <span className="pl-2">Marketplace</span>
         </button>
-        <button className="flex items-center justify-start btn w-full">
+        <button className="flex bg-[#F0F2F5] items-center justify-start btn w-full">
           <img
             className="w-8"
             src="https://i.ibb.co/2n3sMCm/images-3-removebg-preview.png"
             alt=""
           />
-          <span>Video</span>
+          <span className="pl-3">Video</span>
         </button>
-        <button className="flex items-center justify-start btn w-full">
+        <button className="flex bg-[#F0F2F5] items-center justify-start btn w-full">
           <span className="bg-[#c8c9cc] p-3 rounded-full">
             <IoIosArrowDown />
           </span>
-          <span>see more</span>
+          <span className="pl-1">see more</span>
         </button>
       </div>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,14 +5,14 @@ import Section3 from "../../Component/HomeSecrion/Section3";
 
 const Home = () => {
   return (
-    <div className="bg-[#F0F2F5] grid grid-cols-3 h-[100vh] gap-10">
-      <div>
+    <div className="bg-[#F0F2F5] flex h-[100vh] gap-10">
+      <div className="w-72">
         <Section1 />
       </div>
-      <div>
+      <div className="">
         <Section2 />
       </div>
-      <div>
+      <div className="w-72">
         <Section3 />
       </div>
     </div>
